435b8a7e1abbff760d02a9844033b762cae01082
Jonny Lloyd <jonnylloyd@Mac.mynet>
2025-08-20 12:18:55 +0100

    Added changes and removed some files

--- C-Learning-main 2/Mon18thUnitTesting/Calculator.cs
+++
@@ -1,9 +0,0 @@
-namespace Mon18thUnitTesting
-{
-    public class Calculator
-    {
-        public int Add(int x, int y) => x + y;
-        public int Subtract(int x, int y) => x - y;
-        public string FullName(string fName, string lName) => $"{fName} {lName}";
-    }
-}
