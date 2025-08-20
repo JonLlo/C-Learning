3042b6a95fa21998cb759550bb6a443399ded18e
Jonny Lloyd <jonnylloyd@Mac.mynet>
2025-08-20 12:17:41 +0100

    SQL code added

---
+++ C#2/Wed20thSQLnotworking/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+ 
+namespace ConnectingToDB;
+ 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string server = "127.0.0.1,1433"; // can use this instead "localhost,1433"
+        string database = "TestDB";
+        string username = "SA";
+        string pwd = "P455Word";
+ 
+        // string password = Environment.GetEnvironmentVariable("pwd").ToString();
+ 
+        var connectionString = $"Server = {server};TrustServerCertificate=True;Database={database};User Id={username};Password={pwd};";
+        var sql = "SELECT * FROM Inventory";
+        var conn = new SqlConnection(connectionString);
+ 
+        // use the sql query
+        conn.Open();
+        var cmd = new SqlCommand(sql, conn);
+        var reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine($"{reader["name"]}");
+        }
+        reader.Close();
+        conn.Close();
+    }
+}
